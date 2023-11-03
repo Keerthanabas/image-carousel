5f79189327a8336cab15b0cc1af62b3937b69727
Keerthanabas <bkeerthanaeee@gmail.com>
2023-11-03 17:28:59 +0530

    carousel commit

---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const data = 
+     [
+        {
+            "src":"https://picsum.photos/seed/img1/600/400",
+            "alt":"image 1 carousel"
+
+        },
+        {
+            "src":"https://picsum.photos/seed/img1=2/600/400",
+            "alt":"image 2 carousel"
+
+        },
+        {
+            "src":"https://picsum.photos/seed/img1=3/600/400",
+            "alt":"image 3 carousel"
+
+        },
+        {
+            "src":"https://picsum.photos/seed/img4/600/400",
+            "alt":"image 4 carousel"
+
+        }
+    ]
---
+++ src/image.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {data} from "./data"
+import "./image.css";
+import { IconName } from "react-icons/bs";
+import {BsArrowLeftCircleFill,BsArrowRightCircleFill} from "react-icons/bs"
+
+
+const Image = ({data})=>{
+    console.log(data)
+return(
+<div className="image">
+    <BsArrowLeftCircleFill/>
+
+{data.map((item,index)=>{
+    return(
+    <img src={item.src} alt={item.alt} key={index} className="slide"/>
+    )
+})
+}
+<BsArrowRightCircleFill/>
+</div>
+
+    )
+}
+export default Image
