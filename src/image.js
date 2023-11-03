240ad9e24f314ff6e7aaebc29e1a24e14d25a6a0
Keerthanabas <bkeerthanaeee@gmail.com>
2023-11-03 19:15:42 +0530

    updates

--- src/image.js
+++ src/image.js
@@ -2,14 +2,16 @@ import React from 'react';
 // import {data} from "./data"
 import "./image.css";
 //import { IconName } from "react-icons/bs";
-import {BsArrowLeftCircleFill,BsArrowRightCircleFill} from 'bootstrap-icons/react';
+import {BsArrowLeftCircleFill,BsArrowRightCircleFill} from 'react-icons/bs';
+// import "bootstrap-icons/font/bootstrap-icons.css";
+//import 'bootstrap/dist/css/bootstrap.min.css'
 
 
 const Image = ({data})=>{
     console.log(data)
 return(
 <div className="image">
-    <BsArrowLeftCircleFill/>
+    <BsArrowLeftCircleFill  className='arrow arrow-left'/>
 
 {data.map((item,index)=>{
     return(
@@ -17,7 +19,14 @@ return(
     )
 })
 }
-    <BsArrowRightCircleFill/>
+    <BsArrowRightCircleFill className='arrow arrow-right'/>
+    <span className='indicators'>
+        {data.map((item,index)=>{
+            return ( 
+            <button key={index} onClick={null} className='indicator'></button> 
+            );
+        })}
+    </span>
 </div>
 
     )
