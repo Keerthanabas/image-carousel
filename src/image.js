1be250e943c9d3d176434da3bc06b7a56d4bf365
Keerthanabas <bkeerthanaeee@gmail.com>
2023-11-04 14:02:16 +0530

    04.11.2023 works

--- src/image.js
+++ src/image.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 // import {data} from "./data"
 import "./image.css";
 //import { IconName } from "react-icons/bs";
@@ -9,21 +9,30 @@ import {BsArrowLeftCircleFill,BsArrowRightCircleFill} from 'react-icons/bs';
 
 const Image = ({data})=>{
     console.log(data)
+    const[slide,setSlide]=useState(0);
+
+    const nextslide = () =>{
+   setSlide (slide+1);
+    }
+    const previousslide =()=>{
+setSlide(slide-1)
+    }
+    
 return(
 <div className="image">
-    <BsArrowLeftCircleFill  className='arrow arrow-left'/>
+    <BsArrowLeftCircleFill  className='arrow arrow-left'onClick={previousslide}/>
 
 {data.map((item,index)=>{
     return(
-    <img src={item.src} alt={item.alt} key={index} className="slide"/>
+    <img src={item.src} alt={item.alt} key={index} className={slide === index ? "slide" : "slide slide-hidden"}/>
     )
 })
 }
-    <BsArrowRightCircleFill className='arrow arrow-right'/>
+    <BsArrowRightCircleFill className='arrow arrow-right' onClick={nextslide}/>
     <span className='indicators'>
         {data.map((item,index)=>{
             return ( 
-            <button key={index} onClick={null} className='indicator'></button> 
+            <button key={index} onClick={null} className={slide === index ? "indicator" : "indicator indicator-inactive"}></button> 
             );
         })}
     </span>
